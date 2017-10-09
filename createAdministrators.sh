7aefe6b7ae7054a20c13a6a0ed8e46e8949c72b5
Jayy0 <32640140+Jayy0@users.noreply.github.com>
2017-10-09 15:08:14 +0200

    added Notes

--- createAdministrators.sh
+++ createAdministrators.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#creates an admin-account with the name "Administrator" and the password is "password"
 sysadminctl -addUser Administrator -fullName Administrator -password password -admin
 
+#creates an admin-account with the name "admin" and the password is "password"
 sysadminctl -addUser Admin -fullName Admin -password password -admin
 
 exit 0
